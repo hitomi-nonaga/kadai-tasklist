6d9d6d6ac2a6dfbe328631da7e82b31da6d92455
hitomi.nonaga <hitomi.nonaga@car-frontier.com>
2018-05-29 10:51:31 +0900

    Add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Tasks;
+import models.validators.TasksValidator;
 import utils.DBUtil;
 
 /**
@@ -48,12 +51,25 @@ public class CreateServlet extends HttpServlet {
 	        t.setCreated_at(currentTime);
 	        t.setUpdated_at(currentTime);
 
-	        em.getTransaction().begin();
-	        em.persist(t);
-	        em.getTransaction().commit();
-	        request.getSession().setAttribute("flush", "登録が完了しました。");
-	        em.close();
+	        List<String> errors = TasksValidator.validate(t);
+	        if(errors.size() > 0) {
+	            em.close();
 
+	            request.setAttribute("_token", request.getSession().getId());
+	            request.setAttribute("tasks", t);
+	            request.setAttribute("errors", errors);
+
+	            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+	            rd.forward(request, response);
+	        } else {
+	            em.getTransaction().begin();
+	            em.persist(t);
+	            em.getTransaction().commit();
+	            request.getSession().setAttribute("flush", "登録が完了しました。");
+	            em.close();
+
+	            response.sendRedirect(request.getContextPath() + "/index");
+	        }
 	        response.sendRedirect(request.getContextPath() + "/index");
 	    }
 	}
